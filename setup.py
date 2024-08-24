7ee0af969b7417f6515994216e306c39fe2d9479
Sufyanhamza <sufyan.hamza07@gmail.com>
2024-08-24 14:53:17 +0500

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Approval_Prediction",
+    version="0.0.0",
+    author="Sufyanhamza",
+    author_email="sufyan.hamza07@gmail.com",
+    packages=find_packages()
+)
+
